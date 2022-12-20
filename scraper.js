6e2e6303409d29b42c730f4591dafe4835374f52
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-12-20 12:19:27 -0800

    comment out unused code for old project

--- scraper.js
+++ scraper.js
@@ -1,23 +1,28 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const { startBrowser } = require('./lib/services/scraper/browser.js');
-const nodeCron = require('node-cron');
+// const nodeCron = require('node-cron');
 const scraperObject = require('./lib/services/scraper/pageScraper.js');
-const Event = require('./lib/models/Event.js');
+// const Event = require('./lib/models/Event.js');
+
 async function grabScraped() {
   const browser = await startBrowser();
   const events = await scraperObject.scraper(browser);
   try {
-    await Promise.allSettled(events.map((event) => Event.insert(event)));
+    // map through settled promises and insert into db
+    // await Promise.allSettled(events.map((event) => Event.insert(event)));
+    // const settledData = await Promise.allSettled(events);
+    // console.log(settledData);
   } catch (e) {
     console.error(e);
   }
   return events;
 }
 
-nodeCron.schedule('*/1 * * * *', async () => {
-  await grabScraped();
-});
-// grabScraped();
+// nodeCron.schedule('*/5 * * * *', async () => {
+//   await grabScraped();
+// });
+
+grabScraped();
 
 // module.exports = { grabScraped };
