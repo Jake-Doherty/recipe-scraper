ec6599dd92b32d98e214c007dabd65ccbc2e1ff3
Jacob Doherty <doherty.jacob.p@gmail.com>
2023-01-04 11:19:09 -0800

    scraper works for some sites and inserts to db

--- scraper.js
+++ scraper.js
@@ -1,14 +1,19 @@
 const dotenv = require('dotenv');
+const Recipe = require('./lib/models/Recipe.js');
 dotenv.config();
 const { startBrowser } = require('./lib/services/scraper/browser.js');
 // const nodeCron = require('node-cron');
-const scraperObject = require('./lib/services/scraper/pageScraper.js');
-// const Event = require('./lib/models/Event.js');
+const fnScraperObject = require('./lib/services/scraper/site-scrapers/food-network.js');
 
-async function grabScraped() {
+async function grabScraped(url) {
+  console.log('in the grabScraped function');
   const browser = await startBrowser();
-  const events = await scraperObject.scraper(browser);
+  console.log('after starting the browser session');
+
+  const recipe = await fnScraperObject.scraper(browser, url);
+  console.log('after scraper finished', recipe);
   try {
+    await Recipe.insert(recipe);
     // map through settled promises and insert into db
     // await Promise.allSettled(events.map((event) => Event.insert(event)));
     // const settledData = await Promise.allSettled(events);
@@ -16,13 +21,13 @@ async function grabScraped() {
   } catch (e) {
     console.error(e);
   }
-  return events;
+  return recipe;
 }
 
 // nodeCron.schedule('*/5 * * * *', async () => {
 //   await grabScraped();
 // });
 
-grabScraped();
+// grabScraped();
 
-// module.exports = { grabScraped };
+module.exports = { grabScraped };
